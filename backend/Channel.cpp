84f66ddcabfb9d9fb6b30f3ee2f0bf0d1fc270b1
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2024-11-14 12:59:54 -0500

    added channel class

---
+++ backend/Channel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lyini on 11/14/2024.
+//
+
+#include "Channel.h"
---
+++ backend/Channel.h
@@ -0,0 +1,16 @@
+//
+// Created by lyini on 11/14/2024.
+//
+
+#ifndef PROJECT3_CHANNEL_H
+#define PROJECT3_CHANNEL_H
+
+
+
+class Channel {
+
+};
+
+
+
+#endif //PROJECT3_CHANNEL_H
