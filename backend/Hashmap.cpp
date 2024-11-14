32ef213371582949698a50d951cd5c0f24f47ecd
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2024-11-14 13:46:59 -0500

    added skeleton functions for HashMap

---
+++ backend/Hashmap.cpp
@@ -0,0 +1,26 @@
+#include "Hashmap.h"
+#include <iostream>
+#include <vector>
+HashMap::HashMap() {
+
+}
+
+void HashMap::insert(string key, vector<Channel> val) {
+
+}
+
+vector<Channel> HashMap::retrieve(string key) {
+    return vector<Channel>();
+}
+
+int HashMap::size() {
+    return 0;
+}
+
+bool HashMap::empty() {
+    return false;
+}
+
+int HashMap::count(string key) {
+    return 0;
+}
--- backend/Hashmap.h
+++ backend/Hashmap.h
@@ -1,4 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "Channel.h"
 #ifndef PROJECT3_HASHMAP_H
 #define PROJECT3_HASHMAP_H
+using namespace std;
+
+struct hashObj {
+    string key;
+    vector<Channel> val;
+    hashObj* next = nullptr;
+};
+
+class HashMap {
+    int numKeys;
+    int bucketCount;
+    double loadFact;
+    double maxLF = 0.7;
+    vector<hashObj*> buckets;
+
+public:
+    HashMap();
+    void insert(string key, vector<Channel> val = {});
+    vector<Channel> retrieve(string key);
+    int size();
+    bool empty();
+    int count(string key);
+};
 
 #endif //PROJECT3_HASHMAP_H
